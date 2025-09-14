f662c513d4e661d44b4a90aa7665a535cd70ce93
Denis Kokin <denis.kokin@gmail.com>
2025-09-14 13:32:05 +0200

    Refactoring snake factory methods

--- src/snake.rs
+++ src/snake.rs
@@ -17,30 +17,45 @@ pub struct Snake {
     length: u32,
     direction: Direction,
     position: Position,
+    positions: Vec<Position>,
 }
 
 impl Snake {
     pub fn new(len: u32, dir: Direction, pos: Position) -> Snake {
+        let mut positions = Vec::new();
+        let mut cur_pos = pos.clone();
+
+        for i in 1..=len {
+            positions.push(cur_pos.clone());
+            cur_pos = match dir {
+                Direction::UP => cur_pos.down(),
+                Direction::LEFT => cur_pos.right(),
+                Direction::DOWN => cur_pos.up(),
+                Direction::RIGHT => cur_pos.left(),
+            }
+        }
         Snake {
             length: len,
             direction: dir,
             position: pos,
+            positions: positions,
         }
     }
 
-    pub fn new2(pos: Position, segments: Vec<Direction>) -> Result<Snake, String> {
-        segments
+    pub fn new2(dir: Direction, positions: Vec<Position>) -> Result<Snake, String> {
+        positions
             .first()
             .ok_or("Segments cannot be empty".to_string())
-            .map(|s| Snake {
-                length: segments.len() as u32,
-                direction: s.clone(),
-                position: pos,
+            .map(|p| Snake {
+                length: positions.len() as u32,
+                direction: dir,
+                position: p.clone(),
+                positions: positions.clone(),
             })
     }
 
-    pub fn positions(&self) -> Vec<Position> {
-        vec![self.position.clone(), self.position.down().clone(), self.position.clone().down().down()]
+    pub fn len(&self) -> u32 {
+        self.length
     }
 
     pub fn forward(&self) -> Snake {
--- tests/snake_test.rs
+++ tests/snake_test.rs
@@ -1,42 +1,110 @@
-use rustysnake::snake::Direction;
+use rustysnake::snake::Direction::{DOWN, LEFT, RIGHT, UP};
 use rustysnake::snake::Position;
 use rustysnake::snake::Snake;
 
 #[test]
 fn test_eq() {
-    let s = Snake::new(3, Direction::UP, Position::new(1, 2));
-    assert_eq!(s, Snake::new(3, Direction::UP, Position::new(1, 2)));
-    assert_ne!(s, Snake::new(4, Direction::UP, Position::new(1, 2)));
-    assert_ne!(s, Snake::new(3, Direction::DOWN, Position::new(1, 2)));
-    assert_ne!(s, Snake::new(3, Direction::UP, Position::new(1, 1)));
+    let s = Snake::new(3, UP, Position::new(3, 3));
+    assert_eq!(s, Snake::new(3, UP, Position::new(3, 3)));
+    assert_ne!(s, Snake::new(4, UP, Position::new(3, 3)));
+    assert_ne!(s, Snake::new(3, DOWN, Position::new(3, 3)));
+    assert_ne!(s, Snake::new(3, RIGHT, Position::new(3, 3)));
+    assert_ne!(s, Snake::new(3, LEFT, Position::new(3, 3)));
 }
 
 #[test]
 fn test_new() {
-    let s1 = Snake::new(3, Direction::UP, Position::new(1, 2));
-    assert_eq!(s1.positions(), vec![Position::new(1, 2), Position::new(1, 3), Position::new(1, 4)]);
-
-    let s2 = Snake::new(2, Direction::UP, Position::new(1, 2));
-    assert_eq!(s1.positions(), vec![Position::new(1, 2), Position::new(1, 3)]);
-}
-
-#[test]
-fn test_new_snake_segments() {
-    let s = Snake::new2(
-        Position::new(1, 2),
-        vec![Direction::RIGHT, Direction::RIGHT, Direction::RIGHT],
+    let actual1 = Snake::new(3, UP, Position::new(3, 3));
+    let expected1 = Snake::new2(
+        UP,
+        vec![
+            Position::new(3, 3),
+            Position::new(3, 4),
+            Position::new(3, 5),
+        ],
     )
     .unwrap();
-    assert_eq!(s, Snake::new(3, Direction::RIGHT, Position::new(1, 2)));
+    assert_eq!(expected1, actual1);
+
+    let actual2 = Snake::new(3, LEFT, Position::new(3, 3));
+    let expected2 = Snake::new2(
+        LEFT,
+        vec![
+            Position::new(3, 3),
+            Position::new(4, 3),
+            Position::new(5, 3),
+        ],
+    )
+    .unwrap();
+    assert_eq!(expected2, actual2);
+
+    let actual3 = Snake::new(3, DOWN, Position::new(3, 3));
+    let expected3 = Snake::new2(
+        DOWN,
+        vec![
+            Position::new(3, 3),
+            Position::new(3, 2),
+            Position::new(3, 1),
+        ],
+    )
+    .unwrap();
+    assert_eq!(expected3, actual3);
+
+    let actual4 = Snake::new(3, RIGHT, Position::new(3, 3));
+    let expected4 = Snake::new2(
+        RIGHT,
+        vec![
+            Position::new(3, 3),
+            Position::new(2, 3),
+            Position::new(1, 3),
+        ],
+    )
+    .unwrap();
+    assert_eq!(expected4, actual4);
 }
 
 #[test]
-fn test_should_return_error_when_segments_empty() {
-    let r = Snake::new2(Position::new(1, 2), vec![]);
+fn test_new2_should_create_snake_of_proper_length() {
+    let actual1 = Snake::new(3, UP, Position::new(3, 3));
+    assert_eq!(3, actual1.len());
+
+    let actual2 = Snake::new(2, UP, Position::new(3, 3));
+    assert_eq!(2, actual2.len());
+
+    let actual1 = Snake::new(3, LEFT, Position::new(3, 3));
+    assert_eq!(3, actual1.len());
+
+    let actual2 = Snake::new(2, LEFT, Position::new(3, 3));
+    assert_eq!(2, actual2.len());
+
+    let actual1 = Snake::new(3, DOWN, Position::new(3, 3));
+    assert_eq!(3, actual1.len());
+
+    let actual2 = Snake::new(2, DOWN, Position::new(3, 3));
+    assert_eq!(2, actual2.len());
+
+    let actual1 = Snake::new(3, RIGHT, Position::new(3, 3));
+    assert_eq!(3, actual1.len());
+
+    let actual2 = Snake::new(2, RIGHT, Position::new(3, 3));
+    assert_eq!(2, actual2.len());
+}
+
+#[test]
+fn test_new2_should_return_error_when_positions_empty() {
+    let r = Snake::new2(LEFT, vec![]);
 
     assert!(r.is_err());
 }
 
+#[test]
+fn test_new2_should_allow_creating_snake_of_length_1() {
+    let r = Snake::new2(LEFT, vec![Position::new(3, 3)]);
+
+    assert!(r.is_ok());
+}
+
+/*
 #[test]
 fn test_forward() {
     let s1 = Snake::new(3, Direction::UP, Position::new(1, 2));
@@ -107,3 +175,4 @@ fn test_right() {
         s.right().right().right().right()
     );
 }
+*/
